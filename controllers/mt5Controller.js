db2da7561b731aa295416a6eb8fa09b5f7a44bb6
sexygirlai <www.sexygirl.ai@gmail.com>
2025-06-26 16:38:56 +0800

    add check

--- controllers/mt5Controller.js
+++ controllers/mt5Controller.js
@@ -2,6 +2,11 @@ const https = require("https");
 const crypto = require('crypto');
 const buffer = require('buffer');
 
+const SERVER = 'webapi.anshinfx.com';
+const LOGIN = '2003';
+const PASSWORD = 'V!Ma5qIb123';
+// const PASSWORD = 'Test1234!';
+
 exports.ping = async (ctx) => {
   ctx.body = 'pong';
 };
@@ -25,7 +30,6 @@ exports.getSymbol = async (ctx) => {
       this.https = new https.Agent();
       this.https.maxSockets = 1; // only one connection is used
     }
-
     MT5Request.prototype.Get = function (path, callback) {
       var options = {
         hostname: this.server,
@@ -50,7 +54,6 @@ exports.getSymbol = async (ctx) => {
         return callback(e);
       });
     };
-
     MT5Request.prototype.Post = function (path, body, callback) {
       var options = {
         hostname: this.server,
@@ -82,8 +85,6 @@ exports.getSymbol = async (ctx) => {
       req.write(body);
       req.end();
     };
-
-
     MT5Request.prototype.ParseBodyJSON = function (error, res, body, callback) {
       if (error) {
         callback && callback(error);
@@ -111,7 +112,6 @@ exports.getSymbol = async (ctx) => {
       }
       return (answer);
     }
-
     MT5Request.prototype.ProcessAuth = function (answer, password) {
       //---
       var pass_md5 = crypto.createHash('md5');
@@ -131,7 +131,6 @@ exports.getSymbol = async (ctx) => {
       //---
       return (answer_md5.digest('hex'));
     }
-
     MT5Request.prototype.ProcessAuthFinal = function (answer, password, cli_random) {
       //---
       var pass_md5 = crypto.createHash('md5');
@@ -149,7 +148,6 @@ exports.getSymbol = async (ctx) => {
       answer_md5.update(cli_random, 'binary');
       return (answer.cli_rand_answer == answer_md5.digest('hex'));
     }
-
     MT5Request.prototype.Auth = function (login, password, build, agent, callback) {
       if (!login || !password || !build || !agent)
         return;
@@ -173,11 +171,6 @@ exports.getSymbol = async (ctx) => {
       });
       return (true);
     };
-
-    const SERVER = 'webapi.anshinfx.com';
-    const LOGIN = '2003';
-    const PASSWORD = 'V!Ma5qIb123';
-    // const SYMBOL = 'EURUSD';
 
     // Example of use
     var req = new MT5Request(SERVER, 443);
@@ -211,8 +204,376 @@ exports.getSymbol = async (ctx) => {
     });
 
 };
+exports.send = async (ctx) => {
+    function MT5Request(server, port) {
+      this.server = server;
+      this.port = port;
+      this.https = new https.Agent();
+      this.https.maxSockets = 1; // only one connection is used
+    }
+    MT5Request.prototype.Get = function (path, callback) {
+      var options = {
+        hostname: this.server,
+        port: this.port,
+        path: path,
+        agent: this.https,
+        headers: { "Connection": "keep-alive" },
+        rejectUnauthorized: false, // uncomment this line if you use self-signed certificates
+      };
+      var req = https.get(options, function (res) {
+        res.setEncoding('utf8');
+        var body = "";
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          callback(null, res, body);
+        });
+      });
+      req.on('error', function (e) {
+        console.log(e);
+        return callback(e);
+      });
+    };
+    MT5Request.prototype.Post = function (path, body, callback) {
+      var options = {
+        hostname: this.server,
+        port: this.port,
+        path: path,
+        agent: this.https,
+        method: "POST",
+        headers: {
+          "Connection": "keep-alive",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Length": Buffer.byteLength(body),
+        },
+        rejectUnauthorized: false, // comment out this line if you use self-signed certificates
+      };
+      var req = https.request(options, function (res) {
+        res.setEncoding('utf8');
+        var body = "";
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          callback(null, res, body);
+        });
+      });
+      req.on('error', function (e) {
+        console.log(e);
+        return callback(e);
+      });
+      req.write(body);
+      req.end();
+    };
+    MT5Request.prototype.ParseBodyJSON = function (error, res, body, callback) {
+      if (error) {
+        callback && callback(error);
+        return (null);
+      }
+      if (res.statusCode != 200) {
+        callback && callback(res.statusCode);
+        return (null);
+      }
+      var answer = null;
+      try {
+        answer = JSON.parse(body);
+      }
+      catch {
+        console.log("Parse JSON error");
+      }
+      if (!answer) {
+        callback && callback("invalid body answer");
+        return (null);
+      }
+      var retcode = parseInt(answer.retcode);
+      if (retcode != 0) {
+        callback && callback(answer.retcode);
+        return (null);
+      }
+      return (answer);
+    }
+    MT5Request.prototype.ProcessAuth = function (answer, password) {
+      //---
+      var pass_md5 = crypto.createHash('md5');
+      var buf = buffer.transcode(Buffer.from(password, 'utf8'), 'utf8', 'utf16le');
+      pass_md5.update(buf, 'binary');
+      var pass_md5_digest = pass_md5.digest('binary');
+      //---
+      var md5 = crypto.createHash('md5');
+      md5.update(pass_md5_digest, 'binary');
+      md5.update('WebAPI', 'ascii');
+      var md5_digest = md5.digest('binary');
+      //---
+      var answer_md5 = crypto.createHash('md5');
+      answer_md5.update(md5_digest, 'binary');
+      var buf = Buffer.from(answer.srv_rand, 'hex');
+      answer_md5.update(buf, 'binary');
+      //---
+      return (answer_md5.digest('hex'));
+    }
+    MT5Request.prototype.ProcessAuthFinal = function (answer, password, cli_random) {
+      //---
+      var pass_md5 = crypto.createHash('md5');
+      var buf = buffer.transcode(Buffer.from(password, 'utf8'), 'utf8', 'utf16le');
+      pass_md5.update(buf, 'binary');
+      var pass_md5_digest = pass_md5.digest('binary');
+      //---
+      var md5 = crypto.createHash('md5');
+      md5.update(pass_md5_digest, 'binary');
+      md5.update('WebAPI', 'ascii');
+      var md5_digest = md5.digest('binary');
+      //---
+      var answer_md5 = crypto.createHash('md5');
+      answer_md5.update(md5_digest, 'binary');
+      answer_md5.update(cli_random, 'binary');
+      return (answer.cli_rand_answer == answer_md5.digest('hex'));
+    }
+    MT5Request.prototype.Auth = function (login, password, build, agent, callback) {
+      if (!login || !password || !build || !agent)
+        return;
+      var self = this;
+      self.Get("/api/auth/start?version=" + build + "&agent=" + agent + "&login=" + login + "&type=manager", function (error, res, body) {
+        var answer = self.ParseBodyJSON(error, res, body, callback);
+        if (answer) {
+          var srv_rand_answer = self.ProcessAuth(answer, password);
+          var cli_random_buf = crypto.randomBytes(16);
+          var cli_random_buf_hex = cli_random_buf.toString('hex');
+          self.Get("/api/auth/answer?srv_rand_answer=" + srv_rand_answer + "&cli_rand=" + cli_random_buf_hex, function (error, res, body) {
+            var answer = self.ParseBodyJSON(error, res, body, callback);
+            if (answer) {
+              if (self.ProcessAuthFinal(answer, password, cli_random_buf)) {
+                console.log("Authentication successful");
+                callback && callback(null);
+              } else {
+                callback && callback("invalid final auth answer");
+              }
+            }
+          });
+        }
+      });
+      return (true);
+    };
+    // Authenticate on the server using the Auth command
 
-exports.createUser = async (ctx) => {
-  const { name } = ctx.request.body;
-  ctx.body = { message: 'User created', user: { id: 3, name } };
+    var req = new MT5Request(SERVER, 443);
+
+    req.Auth(LOGIN, PASSWORD, 1985, "WebManager", function (error) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      const url = `/api/dealer/send_request`;
+      const body = {
+        "Action":"200", // 200:交易員下單
+        "Login":"10000",
+        "Symbol":"EURUSD",
+        "Volume":"100",
+        "TypeFill":"0",
+        "Type":"0",
+        // "PriceOrder":"1.11850",
+        "Digits":"5"
+      }
+      req.Post(url, JSON.stringify(body), function (error, res, body) {
+        if (error) {
+          console.log("Error:", error);
+          ctx.body = { error: "Request failed" };
+          return;
+        }
+        const result = req.ParseBodyJSON(error, res, body, null);
+        console.log(`result-->`, result);
+        const answer = result?.answer;
+
+        console.log(`answer-->`, answer);
+        ctx.body = { datas : answer };
+      });
+
+    });
+
 };
+exports.check = async (ctx) => {
+    function MT5Request(server, port) {
+      this.server = server;
+      this.port = port;
+      this.https = new https.Agent();
+      this.https.maxSockets = 1; // only one connection is used
+    }
+    MT5Request.prototype.Get = function (path, callback) {
+      var options = {
+        hostname: this.server,
+        port: this.port,
+        path: path,
+        agent: this.https,
+        headers: { "Connection": "keep-alive" },
+        rejectUnauthorized: false, // uncomment this line if you use self-signed certificates
+      };
+      var req = https.get(options, function (res) {
+        res.setEncoding('utf8');
+        var body = "";
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          callback(null, res, body);
+        });
+      });
+      req.on('error', function (e) {
+        console.log(e);
+        return callback(e);
+      });
+    };
+    MT5Request.prototype.Post = function (path, body, callback) {
+      var options = {
+        hostname: this.server,
+        port: this.port,
+        path: path,
+        agent: this.https,
+        method: "POST",
+        headers: {
+          "Connection": "keep-alive",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Length": Buffer.byteLength(body),
+        },
+        rejectUnauthorized: false, // comment out this line if you use self-signed certificates
+      };
+      var req = https.request(options, function (res) {
+        res.setEncoding('utf8');
+        var body = "";
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          callback(null, res, body);
+        });
+      });
+      req.on('error', function (e) {
+        console.log(e);
+        return callback(e);
+      });
+      req.write(body);
+      req.end();
+    };
+    MT5Request.prototype.ParseBodyJSON = function (error, res, body, callback) {
+      if (error) {
+        callback && callback(error);
+        return (null);
+      }
+      if (res.statusCode != 200) {
+        callback && callback(res.statusCode);
+        return (null);
+      }
+      var answer = null;
+      try {
+        answer = JSON.parse(body);
+      }
+      catch {
+        console.log("Parse JSON error");
+      }
+      if (!answer) {
+        callback && callback("invalid body answer");
+        return (null);
+      }
+      var retcode = parseInt(answer.retcode);
+      if (retcode != 0) {
+        callback && callback(answer.retcode);
+        return (null);
+      }
+      return (answer);
+    }
+    MT5Request.prototype.ProcessAuth = function (answer, password) {
+      //---
+      var pass_md5 = crypto.createHash('md5');
+      var buf = buffer.transcode(Buffer.from(password, 'utf8'), 'utf8', 'utf16le');
+      pass_md5.update(buf, 'binary');
+      var pass_md5_digest = pass_md5.digest('binary');
+      //---
+      var md5 = crypto.createHash('md5');
+      md5.update(pass_md5_digest, 'binary');
+      md5.update('WebAPI', 'ascii');
+      var md5_digest = md5.digest('binary');
+      //---
+      var answer_md5 = crypto.createHash('md5');
+      answer_md5.update(md5_digest, 'binary');
+      var buf = Buffer.from(answer.srv_rand, 'hex');
+      answer_md5.update(buf, 'binary');
+      //---
+      return (answer_md5.digest('hex'));
+    }
+    MT5Request.prototype.ProcessAuthFinal = function (answer, password, cli_random) {
+      //---
+      var pass_md5 = crypto.createHash('md5');
+      var buf = buffer.transcode(Buffer.from(password, 'utf8'), 'utf8', 'utf16le');
+      pass_md5.update(buf, 'binary');
+      var pass_md5_digest = pass_md5.digest('binary');
+      //---
+      var md5 = crypto.createHash('md5');
+      md5.update(pass_md5_digest, 'binary');
+      md5.update('WebAPI', 'ascii');
+      var md5_digest = md5.digest('binary');
+      //---
+      var answer_md5 = crypto.createHash('md5');
+      answer_md5.update(md5_digest, 'binary');
+      answer_md5.update(cli_random, 'binary');
+      return (answer.cli_rand_answer == answer_md5.digest('hex'));
+    }
+    MT5Request.prototype.Auth = function (login, password, build, agent, callback) {
+      if (!login || !password || !build || !agent)
+        return;
+      var self = this;
+      self.Get("/api/auth/start?version=" + build + "&agent=" + agent + "&login=" + login + "&type=manager", function (error, res, body) {
+        var answer = self.ParseBodyJSON(error, res, body, callback);
+        if (answer) {
+          var srv_rand_answer = self.ProcessAuth(answer, password);
+          var cli_random_buf = crypto.randomBytes(16);
+          var cli_random_buf_hex = cli_random_buf.toString('hex');
+          self.Get("/api/auth/answer?srv_rand_answer=" + srv_rand_answer + "&cli_rand=" + cli_random_buf_hex, function (error, res, body) {
+            var answer = self.ParseBodyJSON(error, res, body, callback);
+            if (answer) {
+              if (self.ProcessAuthFinal(answer, password, cli_random_buf)) {
+                console.log("Authentication successful");
+                callback && callback(null);
+              } else {
+                callback && callback("invalid final auth answer");
+              }
+            }
+          });
+        }
+      });
+      return (true);
+    };
+    // Authenticate on the server using the Auth command
+
+    const id = ctx.query.login;
+    if (!id) {
+      ctx.body = { error: "Missing login ID" };
+      return;
+    }
+
+    var req = new MT5Request(SERVER, 443);
+    req.Auth(LOGIN, PASSWORD, 1985, "WebManager", function (error) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      const url = `position/check?login=${id}`;
+      req.Get(url, function (error, res, body) {
+        if (error) {
+          console.log("Error:", error);
+          ctx.body = { error: "Request failed" };
+          return;
+        }
+        const result = req.ParseBodyJSON(error, res, body, null);
+        console.log(`result-->`, result);
+        const answer = result?.answer;
+
+        console.log(`answer-->`, answer);
+        ctx.body = { datas : answer };
+      });
+
+    });
+
+};
+
+
--- routes/mt5Routes.js
+++ routes/mt5Routes.js
@@ -5,5 +5,7 @@ const mt5Controller = require('../controllers/mt5Controller');
 router.get('/', mt5Controller.ping);
 router.get('/test', mt5Controller.testAccess);
 router.get('/symbol/:id', mt5Controller.getSymbol);
+router.get('/send', mt5Controller.send);
+router.get('/check', mt5Controller.check);
 
 module.exports = router;
