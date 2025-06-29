5a1250b7fe08c9eddaf38dadaf0da3d1c1643e81
vmixp7 <vmixp7@gmail.com>
2025-06-29 15:36:22 +0800

    add checkpassword

--- controllers/mt5Controller.js
+++ controllers/mt5Controller.js
@@ -6,35 +6,28 @@ const lastTickLibrary = require('../lib/lastTick');
 const checkPositionLibrary = require('../lib/checkPosition');
 const openLibrary = require('../lib/open');
 const closeLibrary = require('../lib/close');
-
-const SERVER = 'webapi.anshinfx.com';
-const LOGIN = '2003';
-const PASSWORD = 'V!Ma5qIb123';
-// const LOGIN = '10000';
-// const PASSWORD = 'Test1234!';
+const checkPasswordLibrary = require('../lib/checkPassword');
 
 exports.ping = async (ctx) => {
   ctx.status = 200;
   ctx.body = 'pong';
-};
+}
 exports.testAccess = async (ctx) => {
   ctx.statusCode = 201;
   ctx.body = [
     { id: 1, name: 'Alice' },
     { id: 2, name: 'Bob' }
   ];
-};
+}
 exports.getSymbol = async (ctx) => {
-
   const result = await symbolLibrary('EURUSD');
-
   ctx.status = 200;
   ctx.body = {
     ok: result,
   }
-
-};
+}
 exports.lastTick = async (ctx) => {
+  try {
     const symbol = ctx.query.symbol;
     if (!symbol) {
       ctx.status = 400;
@@ -47,8 +40,17 @@ exports.lastTick = async (ctx) => {
       code: 0,
       data: result,
     }
-};
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = {
+      code: 1,
+      msg: "Internal Server Error",
+      error: error.message
+    }
+  }
+}
 exports.checkPosition = async (ctx) => {
+  try {
     const login = ctx.query.login;
     if (!login) {
       ctx.status = 400;
@@ -64,44 +66,100 @@ exports.checkPosition = async (ctx) => {
       code: 0,
       data: result,
     }
-};
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = {
+      code: 1,
+      msg: "Internal Server Error",
+      error: error.message
+    }
+  }
+}
 exports.open = async (ctx) => {
-  const login = ctx.query.login;
-  const symbol = ctx.query.symbol;
-  const buyType = ctx.query.type;
-  if (!login || !symbol || !buyType) {
-    ctx.status = 400;
+  try {
+    const login = ctx.query.login;
+    const symbol = ctx.query.symbol;
+    const buyType = ctx.query.type;
+    if (!login || !symbol || !buyType) {
+      ctx.status = 400;
+      ctx.body = {
+        code: 1,
+        msg: "Missing params"
+      };
+      return;
+    }
+    const result = await openLibrary(login, symbol, buyType);
+    ctx.status = 200;
+    ctx.body = {
+      code: 0,
+      data: result,
+    }
+  } catch (error) {
+    ctx.status = 500;
     ctx.body = {
       code: 1,
-      msg: "Missing params"
-    };
-    return;
+      msg: "Internal Server Error",
+      error: error.message
+    }
   }
-  const result = await openLibrary(login, symbol, buyType);
-  ctx.status = 200;
-  ctx.body = {
-    code: 0,
-    data: result,
-  }
-};
+}
 exports.close = async (ctx) => {
-  const login = ctx.query.login;
-  const symbol = ctx.query.symbol;
-  const position = ctx.query.position;
-  if (!login || !symbol || !position) {
-    ctx.status = 400;
+  try {
+    const login = ctx.query.login;
+    const symbol = ctx.query.symbol;
+    const position = ctx.query.position;
+    if (!login || !symbol || !position) {
+      ctx.status = 400;
+      ctx.body = {
+        code: 1,
+        msg: "Missing params"
+      };
+      return;
+    }
+    const result = await closeLibrary(login, symbol, position);
+    ctx.status = 200;
+    ctx.body = {
+      code: 0,
+      data: result,
+    }
+  } catch (error) {
+    ctx.status = 500;
     ctx.body = {
       code: 1,
-      msg: "Missing params"
-    };
-    return;
+      msg: "Internal Server Error",
+      error: error.message
+    }
   }
-  const result = await closeLibrary(login, symbol, position);
-  ctx.status = 200;
-  ctx.body = {
-    code: 0,
-    data: result,
+}
+exports.checkPassword = async (ctx) => {
+  try {
+    const login = ctx.query.login;
+    const paassword = ctx.query.paassword;
+    const mtype = ctx.query.type;
+    if (!login || !mtype || !paassword) {
+      ctx.status = 400;
+      ctx.body = {
+        code: 1,
+        msg: "Missing params"
+      };
+      return;
+    }
+    const result = await checkPasswordLibrary(login, mtype, paassword);
+    ctx.status = 200;
+    let dataStr = 'ok';
+    if (result !== '0 Done') {
+      dataStr = 'error';
+    }
+    ctx.body = {
+      code: 0,
+      data: dataStr,
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = {
+      code: 1,
+      msg: "Internal Server Error",
+      error: error.message
+    }
   }
-};
-
-
+}
