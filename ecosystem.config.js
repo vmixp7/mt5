dd3d66223b1e700fb438d5dbc7b138ed376ea43e
sexygirlai <www.sexygirl.ai@gmail.com>
2025-07-10 18:35:13 +0800

    add log time

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "mt5proxy", // 你可以自訂應用名稱
       script: "app.js", // 你的主程式檔案
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
       node_args: "--max-old-space-size=2048", // 記憶體上限 2GB
       instances: 'max', // 啟動幾個實例（你也可以用 'max' 自動判斷 CPU 核心數）
       autorestart: true,
